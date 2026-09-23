a556d30d850ff8cddeb256e9e32fd1e1091862b0
manurungsara6-pixel <manurungsara6@gmail.com>
2026-09-23 15:06:54 +0700

    Add files via upload

---
+++ src/latihanklineserver.java
@@ -0,0 +1,13 @@
+
+
+public class latihanklineserver {
+
+    public static void main(String[] args) {
+        System.out.println("==============================");
+        System.out.println("Nama  : Sara manurung");
+        System.out.println("NIM   : 25110199");
+        System.out.println("Kelas : TK Grup C");
+        System.out.println("==============================");
+        System.out.println("Aplikasi klien siap dijalankan");
+    }
+}
